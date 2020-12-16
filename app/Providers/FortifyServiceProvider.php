bc742b8e29f714aca3f9660e92713d684013595a
Andre Madarang <andre_madarang@hotmail.com>
2020-12-15 22:37:02 -0500

    Episode 6 - Two Factor Authentication

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -39,6 +39,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::requestPasswordResetLinkView(fn () => view('auth.forgot-password'));
         Fortify::resetPasswordView(fn ($request) => view('auth.reset-password', ['request' => $request]));
         Fortify::confirmPasswordView(fn () => view('auth.confirm-password'));
+        Fortify::twoFactorChallengeView(fn () => view('auth.two-factor-challenge'));
 
         // Fortify::authenticateThrough(function (Request $request) {
         //     return array_filter([
